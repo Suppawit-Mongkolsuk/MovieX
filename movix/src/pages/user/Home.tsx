bdbb9fc813743559b9a4a4fd963edc2b0fae2e95
Suppawit Mongkonsuk <157823980+Suppawit-Mongkolsuk@users.noreply.github.com>
2025-10-13 19:43:01 +0700

    Add Button component
    Introduced a customizable Button component with support for size and variant props in base/Button.tsx.

--- movix/src/pages/user/Home.tsx
+++ movix/src/pages/user/Home.tsx
@@ -1,27 +1,3 @@
-import Card from "../../components/base/Card"
-import { movies } from "../../data/movies"
 
-const Home = () => {
 
-return (
-    <div className="min-h-screen lex justify-center items-center p-10">
-        <h2 className="text-white text-3xl font-bold mb-8 flex items-center gap-2">
-        🎬 Now Showing
-        </h2>
-        <div className="flex flex-wrap justify-center gap-8">
-            {movies.map((movie) => (
-                <Card 
-                    key={movie.id}
-                    title={movie.title}
-                    imageUrl={movie.poster}
-                    date={movie.date}
-                    time={`${movie.time} mins`}
-                    onClick={() => alert(`You clicked on ${movie.title}`)}
-                />
-            ))}
-        </div>
-    </div>
-  )
-}
 
-export default Home
