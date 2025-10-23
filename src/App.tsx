30ad71a46e5b2b24c7de29d977d247036c013fb7
Kobchai-Tar <hayatekungtar@gmail.com>
2025-10-24 03:53:40 +0700

    เพิ่ม footer ของทุกหน้า ละ install react-icons เพิ่ม

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
 import ForgotPasswordForm from "./components/ForgotPassword";
 import { AnimatePresence, motion } from "framer-motion";
+import Footer from "./components/Footer";
 
 const App: React.FC = () => {
   const location = useLocation();
@@ -13,7 +14,7 @@ const App: React.FC = () => {
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<PageWrapper><LoginForm /></PageWrapper>} />
         <Route path="/register" element={<PageWrapper><RegisterForm /></PageWrapper>} />
-        <Route path="/forgot-password" element={<ForgotPasswordForm />} />
+        <Route path="/forgot-password" element={<PageWrapper><ForgotPasswordForm /></PageWrapper>} />
       </Routes>
     </AnimatePresence>
   );
@@ -25,9 +26,9 @@ const App: React.FC = () => {
     animate={{ opacity: 1, x: 0 }}
     exit={{ opacity: 0, x: -50 }}
     transition={{ duration: 0.6, ease: "easeInOut" }}
-    className="min-h-screen w-full"
-  >
-    {children}
+    className="min-h-screen w-full">
+        {children}
+     <Footer />
   </motion.div>
 );
 export default App;
